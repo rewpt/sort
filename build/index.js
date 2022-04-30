5fcd29dbd3e9925dc00687fd319260d2553c624c
rewpt <zpermack@gmail.com>
2022-04-30 16:16:54 -0400

    add logging for sorter2

--- build/index.js
+++ build/index.js
@@ -7,7 +7,6 @@ const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
-//
 const charactersCollection = new CharactersCollection_1.CharactersCollection("xXaYyfBb");
 const sorter2 = new Sorter_1.Sorter(charactersCollection);
 sorter2.sort();
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
-//
+
 const charactersCollection = new CharactersCollection("xXaYyfBb");
 const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();

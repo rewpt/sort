8778ad7d78cc34b21e088619d4ef4c42cfa6c314
rewpt <zpermack@gmail.com>
2022-04-30 16:15:39 -0400

    add example of sorting for a string

--- build/index.js
+++ build/index.js
@@ -2,7 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+//
+const charactersCollection = new CharactersCollection_1.CharactersCollection("xXaYyfBb");
+const sorter2 = new Sorter_1.Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);

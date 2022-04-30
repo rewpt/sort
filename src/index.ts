ec4c9c86656d6bf75556bb5f40c6691599d74c2c
rewpt <zpermack@gmail.com>
2022-04-29 23:24:47 -0400

    import NumbersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { NumbersCollection } from './NumbersCollection';
+
+
 class Sorter {
   constructor(public collection: /*something else */) {}
 
   sort(): void {
     const { length } = this.collection;
-    
+
     for (let i = 0; i < length; i++) {
       // using -1 to keep track of not needing the
       // next ending index in bubble sort after each iteration

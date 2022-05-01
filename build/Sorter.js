6d0187c225a3d5c56f04e4b99ea8191affe19c99
rewpt <zpermack@gmail.com>
2022-05-01 13:39:47 -0400

    add abstract implementation for compare, swap and length for sorter class

--- build/Sorter.js
+++ build/Sorter.js
@@ -1,18 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
+// Sorter is an abstract class, so we can call methods
+// that don't exist yet that we expect to exist in the child class
 class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
     sort() {
-        const { length } = this.collection;
+        const { length } = this;
         for (let i = 0; i < length; i++) {
             // using -1 to keep track of not needing the
             // next ending index in bubble sort after each iteration
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
--- src/Sorter.ts
+++ src/Sorter.ts
@@ -8,18 +8,22 @@ interface Sortable {
   swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+// Sorter is an abstract class, so we can call methods
+// that don't exist yet that we expect to exist in the child class
+export abstract class Sorter {
+  abstract compare(leftIndex: Number, rightIndex: number): boolean;
+  abstract swap(leftIndex: Number, rightIndex: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       // using -1 to keep track of not needing the
       // next ending index in bubble sort after each iteration
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }

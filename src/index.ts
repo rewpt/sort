2380bca89df105145d9ceb9a954820da03de9208
rewpt <zpermack@gmail.com>
2022-05-01 12:49:37 -0400

    add example of sorted linked list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection("xXaYyfBb");
 const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter3 = new Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
